e37017cd620cb83535e162deda8800deea0ac348
Fian Putra <mac@172-125-0-92.lightspeed.rcsntx.sbcglobal.net>
2020-10-13 02:03:35 +0800

    restructure project atomic

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {Home, Login, Register} from "../../pages";
-export default function Router(){
+export default function Routes(){
     return (
-        <div>
             <Router>
                 <Switch>
-                    <Route path="/">
-                        <Home/>
-                    </Route>
+
                     <Route path="/register">
                         <Register/>
                     </Route>
                     <Route path="/login">
                         <Login/>
                     </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
                 </Switch>
             </Router>
-        </div>
     )
 }
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import "./register.scss";
 export default function Register(){
     return (
-        <div>
-            <h3>Register Page</h3>
+        <div className="main-page">
+            <div className="left">
+                <p>Ilustrasi</p>
+            </div>
+            <div className="right">
+                <p>Form Registrasi</p>
+            </div>
         </div>
     )
 }
